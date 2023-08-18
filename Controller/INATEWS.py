eb78f153ca76f7cb0816aefd3b7348886bbf6161
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-18 04:15:11 +0000

    Add code to load json file

--- Controller/INATEWS.py
+++ Controller/INATEWS.py
@@ -1,2 +1,14 @@
-class inatews:
-  pass
+import requests
+
+class ReadUrl:
+    def read_json(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+class INA_TEWS:
+    def news(self):
+        reader = ReadUrl()
+        json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
+        return json_data

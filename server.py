f4be86d95fbf83f5733147622911c39a98e5f810
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-18 04:37:06 +0000

    Add route for inatews-news

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from Controller.INATEWS import INA_TEWS
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+    return render_template("home.html")
 
 @app.route("/inatews-news")
 def inatews_news():
-  return render_template("inatews-news.html")
+    inatews = INA_TEWS()
+    json_data = inatews.news()
+    return render_template("inatews-news.html", json_data=json_data)

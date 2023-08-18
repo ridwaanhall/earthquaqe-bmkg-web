40c4a6c4dbc1fa4a65bbd7b2596bbb82fca1a387
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-18 01:55:38 +0000

    Modify code

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   return render_template("home.html")
+
+@app.route("/inatews-news")
+def inatews_news():
+  return render_template("inatews-news.html")

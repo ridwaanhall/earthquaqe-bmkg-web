f46a418a595c33d3865f7d7c754d8ccd9d9d5bff
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-19 23:31:44 +0000

    Formatted code

--- Controller/EarthquakeController.py
+++ Controller/EarthquakeController.py
@@ -1,52 +1,64 @@
 import requests
 
+
 class ReadUrl:
-    def read_json(self, url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()
-        return None
+
+  def read_json(self, url):
+    response = requests.get(url)
+    if response.status_code == 200:
+      return response.json()
+    return None
+
 
 class INA_TEWS:
-    def news(self):
-        reader = ReadUrl()
-        json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
-        return json_data
 
-    def maps(self):
-        reader = ReadUrl()
-        json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
-        coordinates_str = json_data["info"]["point"]["coordinates"]
-        headline = json_data['info']['headline']
-        longitude, latitude = map(float, coordinates_str.split(','))
-        return longitude, latitude, headline
+  def news(self):
+    reader = ReadUrl()
+    json_data = reader.read_json(
+      'https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
+    return json_data
 
-    #def live30event(self):
-    #    reader = ReadUrl()
-    #    json_data = reader.read_json('https://earthquake-bmkg-api.ridwaanhall.repl.co/live30event.json')
-    #    return json_data
+  def maps(self):
+    reader = ReadUrl()
+    json_data = reader.read_json(
+      'https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
+    coordinates_str = json_data["info"]["point"]["coordinates"]
+    headline = json_data['info']['headline']
+    longitude, latitude = map(float, coordinates_str.split(','))
+    return longitude, latitude, headline
 
-    def live30event(self):
-        reader = ReadUrl()
-        json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/live30event.json')
-        gempa_list = json_data["Infogempa"]["gempa"]
-        total_data = len(json_data["Infogempa"]["gempa"])
-        dalam_values = [float(gempa["dalam"]) for gempa in gempa_list]
-        mag_values = [float(gempa["mag"]) for gempa in gempa_list]
-      
-        average_dalam = sum(dalam_values) / len(dalam_values) if dalam_values else 0
-        average_mag = sum(mag_values) / len(mag_values) if mag_values else 0
-        return json_data, average_dalam, average_mag, total_data
+  #def live30event(self):
+  #    reader = ReadUrl()
+  #    json_data = reader.read_json('https://earthquake-bmkg-api.ridwaanhall.repl.co/live30event.json')
+  #    return json_data
+
+  def live30event(self):
+    reader = ReadUrl()
+    json_data = reader.read_json(
+      'https://earthquaqe-bmkg-api.ridwaanhall.repl.co/live30event.json')
+    gempa_list = json_data["Infogempa"]["gempa"]
+    total_data = len(json_data["Infogempa"]["gempa"])
+    dalam_values = [float(gempa["dalam"]) for gempa in gempa_list]
+    mag_values = [float(gempa["mag"]) for gempa in gempa_list]
+
+    average_dalam = sum(dalam_values) / len(
+      dalam_values) if dalam_values else 0
+    average_mag = sum(mag_values) / len(mag_values) if mag_values else 0
+    return json_data, average_dalam, average_mag, total_data
+
 
 class BMKG_Data:
-    def news(self):
-        reader = ReadUrl()
-        json_data = reader.read_json('https://earthquake-bmkg-api.ridwaanhall.repl.co/autogempa.json')
-        return json_data
 
-    def maps(self):
-        reader = ReadUrl()
-        json_data = reader.read_json('https://earthquaqe-bmkg-api-v1.ridwaanhall.repl.co/autogempa.json')
-        coordinates_str = json_data["Infogempa"]["gempa"]["point"]["coordinates"]
-        latitude, longitude = map(float, coordinates_str.split(','))
-        return latitude, longitude
+  def news(self):
+    reader = ReadUrl()
+    json_data = reader.read_json(
+      'https://earthquake-bmkg-api.ridwaanhall.repl.co/autogempa.json')
+    return json_data
+
+  def maps(self):
+    reader = ReadUrl()
+    json_data = reader.read_json(
+      'https://earthquaqe-bmkg-api-v1.ridwaanhall.repl.co/autogempa.json')
+    coordinates_str = json_data["Infogempa"]["gempa"]["point"]["coordinates"]
+    latitude, longitude = map(float, coordinates_str.split(','))
+    return latitude, longitude
--- server.py
+++ server.py
@@ -3,54 +3,64 @@ from Controller.EarthquakeController import INA_TEWS, BMKG_Data
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "ridwan dimari"
+  return "ridwan dimari"
 
 
 # =========== INATEWS ==========
 @app.route("/inatews-news")
 def inatews_news():
-    inatews = INA_TEWS()
-    json_data = inatews.news()
-    return render_template("inatews-news.html", json_data=json_data)
+  inatews = INA_TEWS()
+  json_data = inatews.news()
+  return render_template("inatews-news.html", json_data=json_data)
 
 
 @app.route("/inatews-maps")
 def inatews_maps():
-    ina_tews = INA_TEWS()
-    longitude, latitude, headline = ina_tews.maps()
-    json_data = ina_tews.news()
-    return render_template("inatews-maps.html", longitude=longitude, latitude=latitude, headline=headline, json_data=json_data)
+  ina_tews = INA_TEWS()
+  longitude, latitude, headline = ina_tews.maps()
+  json_data = ina_tews.news()
+  return render_template("inatews-maps.html",
+                         longitude=longitude,
+                         latitude=latitude,
+                         headline=headline,
+                         json_data=json_data)
 
 
 @app.route("/inatews-live30event")
 def inatews_live30event():
-    inatews = INA_TEWS()
-    json_data, average_dalam, average_mag, total_data = inatews.live30event()
-    average_dalam_formatted = "{:.2f}".format(average_dalam)
-    average_mag_formatted = "{:.2f}".format(average_mag)
-    return render_template("inatews-live30event.html", json_data=json_data, average_dalam=average_dalam, average_mag=average_mag, total_data=total_data)
+  inatews = INA_TEWS()
+  json_data, average_dalam, average_mag, total_data = inatews.live30event()
+  average_dalam_formatted = "{:.2f}".format(average_dalam)
+  average_mag_formatted = "{:.2f}".format(average_mag)
+  return render_template("inatews-live30event.html",
+                         json_data=json_data,
+                         average_dalam=average_dalam,
+                         average_mag=average_mag,
+                         total_data=total_data)
 
 
 # =========== BMKG DATA ==========
 @app.route("/bmkgdata-news")
 def bmkgdata_news():
-    bmkgdata = BMKG_Data()
-    json_data = bmkgdata.news()
-    return render_template("bmkgdata-news.html", json_data=json_data)
+  bmkgdata = BMKG_Data()
+  json_data = bmkgdata.news()
+  return render_template("bmkgdata-news.html", json_data=json_data)
 
 
 @app.route("/bmkgdata-maps")
 def bmkgdata_maps():
-    bmkgdata = BMKG_Data()
-    latitude, longitude = bmkgdata.maps()
-    json_data = bmkgdata.news()
-    return render_template("bmkgdata-maps.html", longitude=longitude, latitude=latitude, json_data=json_data)
-
-
+  bmkgdata = BMKG_Data()
+  latitude, longitude = bmkgdata.maps()
+  json_data = bmkgdata.news()
+  return render_template("bmkgdata-maps.html",
+                         longitude=longitude,
+                         latitude=latitude,
+                         json_data=json_data)
 
 
 @app.route("/test")
 def test():
-    return render_template("testing.html")
+  return render_template("testing.html")

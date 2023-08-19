e72d810f18f573c395e27fe6e2026c6235fc0ad3
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-19 08:52:23 +0000

    Add route for maps

--- server.py
+++ server.py
@@ -13,6 +13,15 @@ def inatews_news():
     json_data = inatews.news()
     return render_template("inatews-news.html", json_data=json_data)
 
+
+@app.route("/inatews-maps")
+def inatews_maps():
+    ina_tews = INA_TEWS()
+    longitude, latitude, headline = ina_tews.maps()
+    json_data = ina_tews.news()
+    return render_template("inatews-maps.html", longitude=longitude, latitude=latitude, headline=headline, json_data=json_data)
+
+
 @app.route("/test")
 def test():
     return render_template("testing.html")

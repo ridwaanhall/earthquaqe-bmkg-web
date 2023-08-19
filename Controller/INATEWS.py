e824e167f167365453d4c32318ed7ad0ebf40604
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-19 08:51:53 +0000

    Add maps

--- Controller/INATEWS.py
+++ Controller/INATEWS.py
@@ -12,3 +12,11 @@ class INA_TEWS:
         reader = ReadUrl()
         json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
         return json_data
+
+    def maps(self):
+      reader = ReadUrl()
+      json_data = reader.read_json('https://earthquaqe-bmkg-api-v1.ridwaanhall.repl.co/new.json')
+      coordinates_str = json_data["info"]["point"]["coordinates"]
+      headline = json_data['info']['headline']
+      longitude, latitude = map(float, coordinates_str.split(','))
+      return longitude, latitude, headline

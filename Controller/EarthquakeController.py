3eabf70d4e14e6a2fc6aa0769e335ee0553064de
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-19 12:18:30 +0000

    Add average

--- Controller/EarthquakeController.py
+++ Controller/EarthquakeController.py
@@ -15,17 +15,28 @@ class INA_TEWS:
 
     def maps(self):
         reader = ReadUrl()
-        json_data = reader.read_json('https://earthquaqe-bmkg-api-v1.ridwaanhall.repl.co/new.json')
+        json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/new.json')
         coordinates_str = json_data["info"]["point"]["coordinates"]
         headline = json_data['info']['headline']
         longitude, latitude = map(float, coordinates_str.split(','))
         return longitude, latitude, headline
 
+    #def live30event(self):
+    #    reader = ReadUrl()
+    #    json_data = reader.read_json('https://earthquake-bmkg-api.ridwaanhall.repl.co/live30event.json')
+    #    return json_data
+
     def live30event(self):
         reader = ReadUrl()
         json_data = reader.read_json('https://earthquaqe-bmkg-api.ridwaanhall.repl.co/live30event.json')
-        return json_data
-
+        gempa_list = json_data["Infogempa"]["gempa"]
+        total_data = len(json_data["Infogempa"]["gempa"])
+        dalam_values = [float(gempa["dalam"]) for gempa in gempa_list]
+        mag_values = [float(gempa["mag"]) for gempa in gempa_list]
+      
+        average_dalam = sum(dalam_values) / len(dalam_values) if dalam_values else 0
+        average_mag = sum(mag_values) / len(mag_values) if mag_values else 0
+        return json_data, average_dalam, average_mag, total_data
 
 class BMKG_Data:
     def news(self):
